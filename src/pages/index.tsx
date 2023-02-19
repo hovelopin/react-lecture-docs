cc00f6556efcab045251bc3d9a33a33a90abe615
hovelopin <kimhojin3714@naver.com>
2023-02-19 15:46:26 +0900

    feat : 불필요한 데이터 제거 및 1주차 초기 셋팅

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+export default function Index(){
+  return (
+    <>
+      <h2>1주차 강의</h2>
+      <ul>
+        <li>useState</li>
+        <li>useEffect</li>
+        <li>useRef</li>
+      </ul>
+    </>
+  )
+}
